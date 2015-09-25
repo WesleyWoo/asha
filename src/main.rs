47f2b4b8b628a2b533f162908a434eaf18d2427e
wuye <wuye@talentwalker.com>
2015-09-25 12:52:45 +0800

    [wesley] add init files

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+
+fn main() {
+    let a = [1, 2, 3, 4];
+    println!("the len of a[] is {}", a.len());
+    for x in 0..a.len(){
+    	println!("index is {} and value is {}", x, a[x]);
+    }
+    //    let stop = 0;
+}
