f02cdbdd877728fed8ed3d556a72e11ee5e45289
wuye <wuye@talentwalker.com>
2015-09-25 16:12:13 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,36 @@
 
 fn main() {
-    
-    let a = [4, 3, 2];
-    let mut index = 0;
-    for i in a.iter(){
-        println!("index is {:?}", index);
-    	println!("i is {:?}", i);
-    	index = index + 1
+    /* ----------------- 范围（Range）迭代器 ----------------- */
+    let mut range = 0..3;
+    println!("range is {:?}", range);
+
+    loop {
+        let result = range.next();
+        if result == None {
+            break;
+        }else {
+            println!("{:?}", result);
+        }
     }
     
-    let shit = 0;
+    /* ----------------- iter迭代器 ----------------- */
+    let a = [3, 4, 7];
+    let mut iter = a.iter();
+    loop {
+        let result = iter.next();
+        if result == None {
+            break;
+        }else {
+            println!("{:?}", result);
+        }
+    }
+    
+    let range_collect = range.collect::<Vec<_>>();
+    println!("range_collect is {:?}", range_collect);
+    
+    let iter_collect = iter.collect::<Vec<_>>();
+    println!("iter_collect is {:?}", iter_collect);
+    
+    
+    let stopHere = 0;
 }
